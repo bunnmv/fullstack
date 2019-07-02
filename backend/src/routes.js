2ba015eadf30985c6f7e8ccc1fd9826a98b2ddba
bunnmv <bunnmv@gmail.com>
2019-07-02 10:45:30 -0300

    Simple get test

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,4 +3,8 @@ const express = require('express');
 const routes = express.Router();
 const userController = require('./controllers/user.controller');
 
+routes.get('/', (req,res)=>{
+    console.log('GET Method');
+    res.sendStatus(200);
+});
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -16,10 +16,10 @@ app.use(require("./routes"));
 
 // For future front-end static files
 // Build files from React.js
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
+//
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-server.listen(3000);
+server.listen(4000);
