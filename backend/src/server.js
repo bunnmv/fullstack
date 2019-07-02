263653d691abe2f08bb2c556ee57c9a369dfeaae
bunnmv <bunnmv@gmail.com>
2019-07-02 11:30:31 -0300

    Include pg to connect with db and improve docker-compose file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const http = require('http');
+
 //middleware to enable CORS. Allow other domains to make requests
 app.use(cors());
 
 const path = require('path');
 
-const server = require('http').Server(app);
+// const server = require('http').Server(app);
 
 app.use(express.json());
 
@@ -22,4 +24,13 @@ app.use(require("./routes"));
 //     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
 
-server.listen(process.env.SERVER_PORT || 4000);
+// server.listen(process.env.SERVER_PORT || 4000);
+
+
+const server = http.createServer(app).listen(process.env.SERVER_PORT || 4000, function (e) {
+    console.log(`● WEB SERVER STARTED on port 4000`);
+});
+
+server.on('error', (e) => {
+    console.log('SERVER ERROR', e);
+});
