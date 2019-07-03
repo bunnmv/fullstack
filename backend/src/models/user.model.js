35870974280bd36118c20cd2b45a4d80a4528e53
bunnmv <bunnmv@gmail.com>
2019-07-02 23:46:24 -0300

    User model to validate input data

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,37 @@
 // User model to validate web client input
+const Ajv = require('ajv');
+const ajv = new Ajv({allErrors: true});
 
+const properties = {
+    name: {
+        type: "string",
+        minLength: 3
+    },
+    phone_number: {
+        type: "string",
+        minLength: 8
+    },
+    email: {
+        format: "email",
+        minLength: 4
+    },
+    cpf: {
+        type: "string",
+        minLength: 11
+    },
+    birth_date: {
+        format: "date"
+    },
+
+};
+
+const schema = {
+    type: "object",
+    required: [ "username", "phone_number","email"],
+    properties: properties
+};
+
+module.exports.validate = ajv.compile(schema);
+module.exports.errors = ajv.errorsText;
 
 
