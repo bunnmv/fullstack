998482d5da8aacad3d9012c522c2d7da796a95ff
bunnmv <bunnmv@gmail.com>
2019-07-03 00:35:43 -0300

    User routes REST API

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,9 +3,6 @@ const express = require('express');
 const routes = express.Router();
 const userController = require('./controllers/user.controller');
 
-
-
-
 /**
  * Rotas Usuários
  */
@@ -22,16 +19,28 @@ routes.get('/user/get/:id', userService.get);
 
 /**
  * Create User
+ * Params: id
  */
-routes.post('/user/create', requiresAdmin, userService.create);
+routes.post('/user/create', userService.create);
 
 /**
  * Remove User
+ * Params: user {object}
+ *  params = {
+ *      user: {
+ *          name: name,
+ *          email: email,
+ *          phone_number: phone_number,
+ *          birth_date: birth_date,
+ *          cpf: cpf
+ *      }
+ *  }
  */
 routes.post('/user/remove/:id',userService.remove);
 
 /**
  * Edit User
+ * Params: id
  */
 routes.post('/user/edit/:id',userService.edit);
 
