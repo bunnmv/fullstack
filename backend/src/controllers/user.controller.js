bc51affc43a3a3360b847c8dced78031e1b2c9cd
bunnmv <bunnmv@gmail.com>
2019-07-05 06:14:33 -0300

    Update user controller to new model

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -29,7 +29,7 @@ UserController.edit = async (id, userEditions) => {
 
 // Query all users from DB
 const getUsers =  async () => { // User list does not require whole data
-    const findAllQuery = 'SELECT * FROM "user" ORDER BY "user".date_added DESC;'
+    const findAllQuery = 'SELECT * FROM "user" ORDER BY "user".date_added DESC;';
     try {
         const { rows } = await db.query(findAllQuery);
         return rows ;
@@ -40,52 +40,10 @@ const getUsers =  async () => { // User list does not require whole data
 
 // Query  user from DB respective to the id passed as parameter
 const getUserById =  async (id) => {
-    // const findUser = 'SELECT * FROM "user" WHERE id = $1';
-    const findUser = 'select\n' +
-        '   json_agg(\n' +
-        '           json_build_object(\n' +
-        '                   \'user\', u,\n' +
-        '                   \'addresses\', addresses,\n' +
-        '                   \'phones\', phones\n' +
-        '               )\n' +
-        '       )"user"\n' +
-        'from "user" u\n' +
-        'left join (\n' +
-        '    select\n' +
-        '        a.user_id,\n' +
-        '        json_agg(\n' +
-        '            json_build_object(\n' +
-        '                \'id\', a.id,\n' +
-        '                \'street\', a.street,\n' +
-        '                \'number\', a.number,\n' +
-        '                \'neighborhood\', a.neighborhood,\n' +
-        '                \'city\', a.city,\n' +
-        '                \'state\', a.state,\n' +
-        '                \'zip_code\', a.zip_code\n' +
-        '                )\n' +
-        '            ) addresses\n' +
-        '    from\n' +
-        '        address a\n' +
-        '    group by user_id\n' +
-        ') a on u.id = a.user_id\n' +
-        'left join (\n' +
-        '    select\n' +
-        '        p.user_id,\n' +
-        '        json_agg(\n' +
-        '            json_build_object(\n' +
-        '                \'id\', p.id,\n' +
-        '                \'number\', p.number,\n' +
-        '                \'type\',p.type\n' +
-        '                )\n' +
-        '            ) phones\n' +
-        '    from\n' +
-        '        phone p\n' +
-        '    group by user_id\n' +
-        ') p on u.id = p.user_id\n' +
-        'WHERE u.id = $1';
+    const findUser = 'SELECT * FROM "user" WHERE id = $1';
     try {
         const { rows } = await db.query(findUser,[id]);
-        return rows[0].user[0] ;
+        return rows[0];
     } catch(error) {
         throw error;
     }
